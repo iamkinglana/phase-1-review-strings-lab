0de734bef184f0188e79382a68785c4699de4f1f
+ <+>
2023-03-13 23:19:39 +0300

    String Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+//curent user
+const currentUser = 'Grace Hopper';
+
+//contains welcome to Flatbook
+const welcomeMessage = "Welcome to Flatbook, " + currentUser+ "!";
+
+//excitedWelcome Message
+
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase()+ "!";
+
+//short greeting
+
+let shortGreeting = "Welcome, "+ currentUser[0]+"!";
